794ca76439ec649727b6bf0e35c84384d57c20c9
LEndhoven <leon.end@gmail.com>
2021-10-04 17:05:05 +0200

    Started with DateSelector

--- src/app/hour-entry/date-selector/date-selector.component.ts
+++ src/app/hour-entry/date-selector/date-selector.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from 'ngx-typesafe-forms';
 
 @Component({
   selector: 'app-date-selector',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./date-selector.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DateSelectorComponent {}
+export class DateSelectorComponent {
+  public readonly dateControl = new FormControl<Date>(new Date());
+}
--- src/app/hour-entry/date-selector/date-selector.module.ts
+++ src/app/hour-entry/date-selector/date-selector.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { DateSelectorComponent } from './date-selector.component';
 
 @NgModule({
@@ -11,8 +13,10 @@ import { DateSelectorComponent } from './date-selector.component';
     FormsModule,
     ReactiveFormsModule,
 
+    MatButtonModule,
     MatDatepickerModule,
     MatFormFieldModule,
+    MatIconModule,
   ],
   exports: [DateSelectorComponent],
   declarations: [DateSelectorComponent],
