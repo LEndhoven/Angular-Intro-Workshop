68f8dadb507f7519da5ee25f45fcec1296d915d2
LEndhoven <leon.end@gmail.com>
2021-10-04 15:46:10 +0200

    Added Angular Material

--- src/app/hour-entry/date-selector/date-selector.module.ts
+++ src/app/hour-entry/date-selector/date-selector.module.ts
@@ -1,9 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatFormFieldModule } from '@angular/material/form-field';
 import { DateSelectorComponent } from './date-selector.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // MatDatepickerModule,
+    // MatFormFieldModule,
+  ],
   exports: [DateSelectorComponent],
   declarations: [DateSelectorComponent],
 })
--- src/app/hour-entry/hour-entry.module.ts
+++ src/app/hour-entry/hour-entry.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DateSelectorModule } from './date-selector/date-selector.module';
 import { HourEntryComponent } from './hour-entry.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, DateSelectorModule],
   exports: [HourEntryComponent],
   declarations: [HourEntryComponent],
 })
