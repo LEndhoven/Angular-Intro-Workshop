b58bd22a035de3feb7cdef1d699f9d5de4279fd4
LEndhoven <leon.end@gmail.com>
2021-10-04 14:54:54 +0200

    Cleanup + Settings modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, VERSION } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent {}
