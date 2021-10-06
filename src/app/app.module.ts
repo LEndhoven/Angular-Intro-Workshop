64742cbd1a7c2344b2cc8519a0ca99f9a075bd03
LEndhoven <leon.end@gmail.com>
2021-10-06 17:09:39 +0200

    Added some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HourEntryModule } from './hour-entry/hour-entry.module';
 import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatNativeDateModule, HourEntryModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatNativeDateModule,
+    HourEntryModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
--- src/app/hour-entry/date-selector/date-selector.module.ts
+++ src/app/hour-entry/date-selector/date-selector.module.ts
@@ -1,22 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { DateSelectorComponent } from './date-selector.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
 
     MatButtonModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
   ],
   exports: [DateSelectorComponent],
   declarations: [DateSelectorComponent],
