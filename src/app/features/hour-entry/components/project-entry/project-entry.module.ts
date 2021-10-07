07659a711fefd065b405859c3d6fb530a9090349
LEndhoven <leon.end@gmail.com>
2021-10-07 11:56:00 +0200

    Updated imports

--- src/app/features/hour-entry/components/project-entry/project-entry.module.ts
+++ src/app/features/hour-entry/components/project-entry/project-entry.module.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { ProjectEntryComponent } from './project-entry.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [ProjectEntryComponent],
   declarations: [ProjectEntryComponent],
 })
