88793466919fceb42118ede7deb5844be0339772
LEndhoven <leon.end@gmail.com>
2021-10-07 09:28:49 +0200

    Updated Date settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,33 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HourEntryModule } from './hour-entry/hour-entry.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
-import { MatDateFormats, MAT_DATE_FORMATS } from '@angular/material/core';
-
-export const CUSTOM_MAT_LUXON_DATE_FORMATS: MatDateFormats = {
-  parse: {
-    dateInput: 'dd-MM-yyyy',
-  },
-  display: {
-    dateInput: 'dd-MM-yyyy',
-    monthYearLabel: 'MMM yyyy',
-    dateA11yLabel: 'DDD',
-    monthYearA11yLabel: 'MMMM yyyy',
-  },
-};
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatLuxonDateModule,
+    MatNativeDateModule,
     HourEntryModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  providers: [
-    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_MAT_LUXON_DATE_FORMATS },
-  ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
 })
 export class AppModule {}
--- src/app/hour-entry/date-selector/date-selector.component.ts
+++ src/app/hour-entry/date-selector/date-selector.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormControl } from 'ngx-typesafe-forms';
-import { DateTime } from 'luxon';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-date-selector',
@@ -8,6 +15,41 @@ import { DateTime } from 'luxon';
   styleUrls: ['./date-selector.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DateSelectorComponent {
-  public readonly dateControl = new FormControl<DateTime>(DateTime.local());
+export class DateSelectorComponent implements OnInit, OnDestroy {
+  @Output() public readonly currentDate = new EventEmitter<Date>();
+
+  public readonly dateControl = new FormControl<Date>(new Date());
+
+  private readonly subscriptions = new Subscription();
+
+  public ngOnInit(): void {
+    this.subscriptions.add(
+      this.dateControl.value$.subscribe((currentDate) =>
+        this.currentDate.emit(currentDate)
+      )
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  public goToPreviousDay(): void {
+    const currentDate = this.dateControl.value;
+    const newDate = addDays(currentDate, -1);
+    this.dateControl.setValue(newDate);
+  }
+
+  public goToNextDay(): void {
+    const currentDate = this.dateControl.value;
+    const newDate = addDays(currentDate, 1);
+    this.dateControl.setValue(newDate);
+  }
+}
+
+function addDays(date: Date, days: number): Date {
+  const dateCopy = new Date(date);
+  dateCopy.setDate(date.getDate() + days);
+
+  return dateCopy;
 }
