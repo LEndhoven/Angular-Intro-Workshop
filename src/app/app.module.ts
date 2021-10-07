074dc66515f48eaa5c9680d39c6c260ccd427eac
LEndhoven <leon.end@gmail.com>
2021-10-07 08:40:04 +0200

    Test adding Luxon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,34 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HourEntryModule } from './hour-entry/hour-entry.module';
-import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
+import { MatDateFormats, MAT_DATE_FORMATS } from '@angular/material/core';
+
+export const CUSTOM_MAT_LUXON_DATE_FORMATS: MatDateFormats = {
+  parse: {
+    dateInput: 'dd-MM-yyyy',
+  },
+  display: {
+    dateInput: 'dd-MM-yyyy',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'DDD',
+    monthYearA11yLabel: 'MMMM yyyy',
+  },
+};
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatNativeDateModule,
+    MatLuxonDateModule,
     HourEntryModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_MAT_LUXON_DATE_FORMATS },
+  ],
 })
 export class AppModule {}
--- src/app/hour-entry/date-selector/date-selector.component.ts
+++ src/app/hour-entry/date-selector/date-selector.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl } from 'ngx-typesafe-forms';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-date-selector',
@@ -8,5 +9,5 @@ import { FormControl } from 'ngx-typesafe-forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DateSelectorComponent {
-  public readonly dateControl = new FormControl<Date>(new Date());
+  public readonly dateControl = new FormControl<DateTime>(DateTime.local());
 }
