28fa1af6bd546f074a6f881ebf7f12fa4978aed6
LEndhoven <leon.end@gmail.com>
2021-11-05 09:51:26 +0100

    Removed reference in Module

--- src/app/features/hour-entry/hour-entry.module.ts
+++ src/app/features/hour-entry/hour-entry.module.ts
@@ -4,7 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { TimeFormatPipeModule } from '../../shared/pipes/time-format-pipe.module';
-import { AddProjectEntryModule } from './components/add-project-entry/add-project-entry.module';
 
 import { DateSelectorModule } from './components/date-selector/date-selector.module';
 import { ProjectEntryModule } from './components/project-entry/project-entry.module';
